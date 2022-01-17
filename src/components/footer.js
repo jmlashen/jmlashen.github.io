c3a179a0844dde991dafc05e7ffc1d3366b0c101
Jake Lashenik <jmlashen@gmail.com>
2022-01-16 21:28:59 -0600

    commit to merge

--- src/components/footer.js
+++ src/components/footer.js
@@ -24,6 +24,21 @@ export const Footer = () => {
                 </h3>
             </div>
             </div>
+
+            <div className="footer-container-bottom">
+            <div className="footer-icons">
+                <a target="_blank" href="https://github.com/jmlashen"><img className="github" src={git} alt="footer image" /></a>
+                <a href = "mailto: jmlashen@gmail.com"><img className="mail" src={mail} alt="footer image" /></a>
+                <a target="_blank" href="https://www.linkedin.com/in/jake-lashenik/"><img className="linkedin" src={linkedin} alt="footer image" /></a>
+                
+              
+            </div>
+            <div>
+                <h3>
+                    this website is not a template - designed and developed by me, for you!
+                </h3>
+            </div>
+            </div>
             
         </>
     )
