819c57314a55ce058e38651c04d2f12cdaa5f2c1
Jake Lashenik <jmlashen@gmail.com>
2022-01-18 12:32:46 -0600

    added disclamier for moblie view

--- src/App.js
+++ src/App.js
@@ -21,26 +21,37 @@ export const App = () => {
       <div className='navbar-container'>
         <NavBarBS id="App" />
       </div>
+      
       <div className="disclaimer"><h6 className='disclaimer-text'>Check back soon for mobile view. Best viewed on desktop. Thanks for your paitence!</h6></div>
+      
       <div className='landcontainer'>
         <div className='jake-logo-containter'>
           <img className="jakelogo" src={JakeLogo} alt="C51"></img>
         </div>
+        
+        <div className="disclaimer"><h6 className='disclaimer-text'>Check back soon for mobile view. Best viewed on desktop. Thanks for your paitence!</h6></div>
+      
         <div className='main-video-container'>
           <Video VideoSource="https://res.cloudinary.com/djnk0ey5p/video/upload/v1642173955/Jake_intro_g2ufsq.mp4" />
         </div>
       </div>
       <div className='footer-container'>
+
+      <div className="disclaimer"><h6 className='disclaimer-text'>Check back soon for mobile view. Best viewed on desktop. Thanks for your paitence!</h6></div>
         <IconsTop />
       </div>
+
+      <div className="disclaimer"><h6 className='disclaimer-text'>Check back soon for mobile view. Best viewed on desktop. Thanks for your paitence!</h6></div>
+
       <div>
         <About />
       </div>
 
+      <div className="disclaimer"><h6 className='disclaimer-text'>Check back soon for mobile view. Best viewed on desktop. Thanks for your paitence!</h6></div>
 
       <Work className="workarea" />
 
-
+      <div className="disclaimer"><h6 className='disclaimer-text'>Check back soon for mobile view. Best viewed on desktop. Thanks for your paitence!</h6></div>
      
       <div className='footer-container'>
         <Footer />
