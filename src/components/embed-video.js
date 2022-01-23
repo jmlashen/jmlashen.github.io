9b5a5872468ed737f0016dfbd09b784951c1074c
Jake Lashenik <jmlashen@gmail.com>
2022-01-23 13:07:00 -0600

    video change

--- src/components/embed-video.js
+++ src/components/embed-video.js
@@ -6,7 +6,7 @@ export const EmbedVideo = () => {
 
 
 
-<iframe src='https://res.cloudinary.com/djnk0ey5p/video/upload/v1642964188/sigzkj5cvmvoofabnafx.mp4'
+<iframe src='https://res.cloudinary.com/djnk0ey5p/video/upload/v1642964781/qyclti2k7kr1vo5m6zdf.mp4'
         frameborder='0'
         allow='autoplay; encrypted-media'
         allowfullscreen
