6e38869723fb3fa4b32f160d201a1dfe6f1a8d42
Jake Lashenik <jmlashen@gmail.com>
2022-02-25 20:04:59 -0600

    clean up about section

--- src/components/about.js
+++ src/components/about.js
@@ -21,7 +21,8 @@ export const About = () => {
 
             <div className="aboutsection">
 
-                <div className="about-hi-main">
+                <div className="about-text-main">
+
                     <div className="hiimjake">
 
                         <div><h1>Hi, I'm Jake!</h1></div>
@@ -33,32 +34,29 @@ export const About = () => {
                         </div>
                     </div>
                     <div className="aboutgroup">
-                            <img className="aboutgroup-img" src={group} alt="c51 logo" />
-                        </div>
-                </div>
+                        <img className="aboutgroup-img" src={group} alt="c51 logo" />
+                    </div>
 
+                    
 
-
-
-                <div className="about-text-main">
                     <div><img className="techtool" src={techToolsMobile} alt="computer" /></div>
                     <div><h1>Tech and Tools</h1></div>
                     <p className="paragraphs">I enjoy the challenge and ongoing learning process of being a desinger and developer. I have established a solid foundation with JavaScript and React. My desgin and styling skills are achieved through my knowledge of CSS, SASS, Figma, and Photoshop.
                         I also have experience building CRUD-based applications using ERDs, JSON server, fetch calls, testing APIs using Postman, and the best practices when using Git/GitHub.</p>
-                </div>
+                    <hr></hr>
 
 
-                <div className="about-text-main">
+
                     <div><img className="artabout-uiux" src={uiux} alt="uiux logo" /></div>
                     <div><h1>UI/UX</h1></div>
                     <p className="paragraphs">I find myself naturally drawn to the user experience and client-side. With my experiences in collaboration and leadership as an educator, I have always enjoyed the intrinsic rewards of helping others achieve their goals and overcome challenges.
                         The Nashville Software School's program challenged our small group to create the Cohort's website. We gained valuable experience learning the principles and processes of UI/UX. We even revealed a pain point in the School curriculum relating to graduation requirements during the process! From wireframes to prototypes, user testing, informational architecture, and visual hierarchy–we got our hands dirty with every step.
                         <b> Based on research and data, here's two examples of personas that helped me create this portfolio:</b></p>
                     <div><img className="artabout-personas" src={personas} alt="c51 logo" /></div>
-                </div>
 
 
-                <div className="about-text-main">
+
+
                     <div><img className="artabout-mobile" src={art2} alt="artwork" /></div>
                     <div><h1 >Art and Design</h1></div>
                     <p className="paragraphs">My background in design comes from working as a freelance graphic designer. I mostly design album art for several musical artists, including Jason Wade, of Lifehouse.
