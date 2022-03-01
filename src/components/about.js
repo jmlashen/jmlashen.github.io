d7a584dc5f436d2ffdf376efc5803ead019209f9
Jake Lashenik <jmlashen@gmail.com>
2022-03-01 16:21:26 -0600

    begin branch

--- src/components/about.js
+++ src/components/about.js
@@ -20,18 +20,20 @@ export const About = () => {
 
             <div className="abouthead-img"><img className="aboutme-img" src={aboutme} alt="about" /></div>
             <div className="topabout"></div>
-
+           
 
             <div className="aboutsection">
 
                 <div className="about-white">
                     <div className="hijake1">
                     </div>
+
                     <div className="jakecloud">
                         <div className="highimjakepro"><img className="hihimjakepro-img" src={aboutPro} alt="me" /></div>
                         <div className="highimjakebubs"><img className="hihimjakepro-img" src={bubbs} alt="me" /></div>
                         
-                    </div>
+                    </div>                   
+                
                     <div className="coll"><img className="coll-img" src={coll} alt="me" /></div>
                     <div className="header"><h1>My colleauges have described me as:</h1></div>
                     
@@ -69,10 +71,12 @@ export const About = () => {
                             </li>
                         </ul>
                         </div>
+                        </div>
                        
-                    </div>
-                    <div className="header"><h1>Tech and Tools</h1></div>        
+                   
                     <div><img className="techtool" src={techToolsMobile} alt="computer" /></div>
+                    <div className="header"><h1>Tech and Tools</h1></div>        
+                   
                     <div className="white-box">
                         <p className="paragraphs">I enjoy the challenge and ongoing learning process of being a desinger and developer. I have established a solid foundation with JavaScript and React. My desgin and styling skills are achieved through my knowledge of CSS, SASS, Figma, and Photoshop.
                             I also have experience building CRUD-based applications using ERDs, JSON server, fetch calls, testing APIs using Postman, and the best practices when using Git/GitHub.</p>
@@ -82,8 +86,9 @@ export const About = () => {
                     </div>
 
 
-                    <div className="header"><h1>UI/UX</h1></div>
+                    
                     <div><img className="artabout-uiux" src={guitar} alt="uiux logo" /></div>
+                    <div className="header"><h1>UI/UX</h1></div>
                     <div className="white-box">
                         <div>
                             <p className="paragraphs">I find myself naturally drawn to the user experience and client-side. With my experiences in collaboration and leadership as an educator, I have always enjoyed the intrinsic rewards of helping others achieve their goals and overcome challenges.
@@ -96,8 +101,9 @@ export const About = () => {
                     </div>
 
 
-                    <div className="header"><h1 >Art and Design</h1></div>
+                   
                     <div><img className="techtool" src={artboard} alt="computer" /></div>
+                    <div className="header"><h1 >Art and Design</h1></div>
                     <div className="white-box">
                         <p className="paragraphs">My background in design comes from working as a freelance graphic designer.
                             Through these interactions, I have gained experience passionately working with clients to best represent their ideas with a finished product we can both be proud of. </p>
